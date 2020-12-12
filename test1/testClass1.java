a821c50b39001939a89c47b1416445bf9c8ff6ed
MihneaVamesu <75744934+MihneaVamesu@users.noreply.github.com>
2020-12-12 18:03:59 +0200

    Add files via upload

---
+++ test1/testClass1.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class testClass1 {
+	
+	public boolean isPrime(int n) {
+		
+		for(int i=2; i<n;i++) {
+			if(n%i==0 && i!=n)  
+				return false;
+		}
+		return true;
+	}
+	
+	public int fib(int n) {
+		if(n==0 || n==1)
+			return n;
+		else
+			return fib(n-1) + fib(n-2);
+	}
+	
+	int searchIndex(int searchElem) {
+		int[] numbers = {3, 22, 27, 47, 57, 67, 89, 91, 95, 99, 73, 482, 11, 1, 21, 33,3};
+		int index = Arrays.binarySearch(numbers, searchElem);
+		return index;
+	}
+}
---
+++ test2/testClass2.java
@@ -0,0 +1,23 @@
+public class testClass2 {
+	private String carModel; 
+	private String color;
+	boolean isElectric;
+	
+	testClass2(String carModel, String color, boolean isElectric){
+		this.carModel = carModel;
+		this.color = color;
+		this.isElectric = isElectric;	 
+	}
+	
+	public String getCarModel() {
+		return this.carModel;
+	}
+	
+	public String getColor() {
+		return this.color;
+	}
+	 
+	public boolean getIsElectric() {
+		return this.isElectric;
+	}
+}
